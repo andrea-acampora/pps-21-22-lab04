ece4b4ce51f41fed2b3f1019c7fc9b77c84907e8
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-21 11:58:16 +0100

    feat: create methods for Student class

--- src/main/scala/u04lab/code/Student.scala
+++ src/main/scala/u04lab/code/Student.scala
@@ -5,37 +5,30 @@ import List.*
 trait Student:
   def name: String
   def year: Int
-  def enrolling(course: Course): Unit // the student participates to a Course
+  def enrolling(course: Course*): Unit // the student participates to a Course
   def courses: List[String] // names of course the student participates to
   def hasTeacher(teacher: String): Boolean // is the student participating to a course of this teacher?
+
 
 trait Course:
   def name: String
   def teacher: String
 
 object Student:
-  def apply(name: String, year: Int = 2017): Student = ???
+  def apply(name: String, year: Int = 2017): Student = StudentImpl(name, year)
+  private case class StudentImpl(override val name: String,
+                                 override val year: Int) extends Student:
+
+     private var  _courses: List[Course] = Nil()
+     override def enrolling(courses: Course*): Unit = courses.map(Cons(_, Nil())).foreach(c => _courses = append(_courses, c))
+     override def courses : List[String] = map(_courses)( _.name )
+     override def hasTeacher(teacher: String): Boolean = contains(map(_courses)(x => x.teacher), teacher)
 
 object Course:
-  def apply(name: String, teacher: String): Course = ???
+  def apply(name: String, teacher: String): Course = CourseImpl(name, teacher)
+  private case class CourseImpl(override val name: String,
+                                override val teacher: String) extends Course
 
-@main def checkStudents(): Unit =
-  val cPPS = Course("PPS", "Viroli")
-  val cPCD = Course("PCD", "Ricci")
-  val cSDR = Course("SDR", "D'Angelo")
-  val s1 = Student("mario", 2015)
-  val s2 = Student("gino", 2016)
-  val s3 = Student("rino") // defaults to 2017
-  s1.enrolling(cPPS)
-  s1.enrolling(cPCD)
-  s2.enrolling(cPPS)
-  s3.enrolling(cPPS)
-  s3.enrolling(cPCD)
-  s3.enrolling(cSDR)
-  println(
-    (s1.courses, s2.courses, s3.courses)
-  ) // (Cons(PCD,Cons(PPS,Nil())),Cons(PPS,Nil()),Cons(SDR,Cons(PCD,Cons(PPS,Nil()))))
-  println(s1.hasTeacher("Ricci")) // true
 
 /** Hints:
   *   - simply implement Course, e.g. with a case class
