bf5e2bbc52f522825c4a79ad806dac6b0c184c3f
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-21 10:47:20 +0100

    feat: create case class as companion object for Complex

--- src/main/scala/u04lab/code/Complex.scala
+++ src/main/scala/u04lab/code/Complex.scala
@@ -7,14 +7,10 @@ trait Complex:
   def *(c: Complex): Complex // should implement the product of two complex numbers
 
 object Complex:
-  def apply(re: Double, im: Double): Complex = ??? // Fill here
-
-@main def checkComplex(): Unit =
-  val a = Array(Complex(10, 20), Complex(1, 1), Complex(7, 0))
-  val c = a(0) + a(1) + a(2)
-  println((c, c.re, c.im)) // (ComplexImpl(18.0,21.0),18.0,21.0)
-  val c2 = a(0) * a(1)
-  println((c2, c2.re, c2.im)) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+  def apply(re: Double, im: Double): Complex = ComplexImpl(re, im)
+  private case class ComplexImpl(override val re: Double, override val im: Double) extends Complex :
+    override def +(c: Complex): Complex = ComplexImpl(re + c.re, im + c.im)
+    override def *(c: Complex): Complex = ComplexImpl(re * c.re - im * c.im, re * c.im + im * c.re)
 
 /** Hints:
   *   - implement Complex with a ComplexImpl class, similar to PersonImpl in slides
