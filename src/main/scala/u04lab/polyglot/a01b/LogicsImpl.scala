bffeeb0b3a200112e9f0b1045e69fd94d5f5326a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-28 09:41:08 +0200

    refactor: remove useless _ and improve some methods

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -11,27 +11,24 @@ import u04lab.code.Option.{Some, None}
 import u04lab.code.Stream.{take, toList, iterate}
 import u04lab.code.List.{flatMap, filter, contains, append, length, Cons, Nil}
 
-case class Pair[A](x: A, y: A)
-
 class LogicsImpl(private val size: Int, private val numberOfMines: Int) extends Logics:
 
-  private val _minesStream: Stream[Pair[Int]] = Stream.iterate(Pair(nextInt(size), nextInt(size)))(mine => {
-    var newMine: Pair[Int] = Pair( nextInt(size) , nextInt(size))
-    while (mine eq newMine) newMine = Pair(nextInt(size), nextInt(size)) ; newMine })
-
-  private val _mines: List[Pair[Int]] = toList(take(_minesStream)(numberOfMines))
-  private var _selected: List[Pair[Int]] = Nil()
+  private val minesStream: Stream[(Int, Int)] = iterate(nextInt(size), nextInt(size))( _ => (nextInt(size), nextInt(size)))
+  private val mines: List[(Int, Int)] = toList(take(minesStream)(numberOfMines))
+  private var selected: List[(Int, Int)] = Nil()
 
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    if contains(_mines, Pair(x, y)) then OptionToOptional(None())
+    println(mines)
+    if contains(mines, (x, y)) then
+      OptionToOptional(None())
     else
-      _selected = append(_selected, Cons(Pair(x, y), Nil()))
-      OptionToOptional(Some(minesInNeighbours(Pair(x, y))))
+      selected = append(selected, Cons((x, y), Nil()))
+      OptionToOptional(Some(minesInNeighbours((x, y))))
 
-  def won: Boolean = length(_selected) + numberOfMines == size * size
+  def won: Boolean = length(selected) + numberOfMines == size * size
 
   private def sameAxisNeighbours(axis: Int): List[Int] = toList(take(Stream.iterate(axis - 1)(_ + 1))(3))
 
-  private def minesInNeighbours(value: Pair[Int]): Int =
-    val allNeighbours: List[Pair[Int]] = flatMap(sameAxisNeighbours(value.x))(x => flatMap(sameAxisNeighbours(value.y))(y => Cons(Pair(x, y), Nil())))
-    length(filter(allNeighbours)(contains(_mines, _)))
+  private def minesInNeighbours(value: (Int, Int)): Int =
+    val allNeighbours: List[(Int, Int)] = flatMap(sameAxisNeighbours(value._1 ))(x => flatMap(sameAxisNeighbours(value._2))(y => Cons((x, y), Nil())))
+    length(filter(allNeighbours)(contains(mines, _)))
